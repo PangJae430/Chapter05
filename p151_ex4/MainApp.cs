d6b31b5c5c42c577640aa92fc8a33c1d8be1c8d8
광재 이 <leekj0430@naver.com>
2023-03-13 15:50:37 +0900

    2023-03-13/15:50

--- p151_ex4/MainApp.cs
+++ p151_ex4/MainApp.cs
@@ -11,8 +11,15 @@ namespace p151_ex4
             string input = Console.ReadLine();
             int number = Convert.ToInt32(input);
 
-            if(number % 3 == 0)
-            { Console.WriteLine("")}
+            if (number % 3 == 0)
+            {
+                Console.WriteLine($"입력하신 정수 {number} 은(는) 3의 배수 입니다.");
+            }
+            else
+            { 
+                Console.WriteLine($"입력하신 정수 {number} 은(는) 3의 배수가 아닙니다."); 
+            }
+
         }
     }
 }
