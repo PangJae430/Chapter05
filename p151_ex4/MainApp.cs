ebdc3914f893dbcd5f29927397e577698939ac7f
광재 이 <leekj0430@naver.com>
2023-03-13 15:38:54 +0900

    2023-03-13/15:38

---
+++ p151_ex4/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p151_ex4
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("정수 한 개를 입력하세요 : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if(number % 3 == 0)
+            { Console.WriteLine("")}
+        }
+    }
+}
